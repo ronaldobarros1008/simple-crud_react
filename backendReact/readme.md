e975f47d385ba81141a2b4ccfbf25433ed6fe00a
ronaldobarros1008 <ronaldobarros1008@gmail.com>
2022-09-21 23:02:03 -0300

    Project Created

---
+++ backendReact/readme.md
@@ -0,0 +1,8 @@
+## JSON SERVER
+
+Para rodar o backend de teste (JSON Server):
+
+### `npm start`
+
+Dentro da pasta **backendReact**
+Abra [http://localhost:3001](http://localhost:3001) para ver os dados no navegador.
---
+++ frontendReact/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Desenvolvido com <i className="fa fa-gear text-danger"></i> por
+            <strong> RB<span className="text-danger">Tec</span></strong>
+        </span>
+    </footer>
